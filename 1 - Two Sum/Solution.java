6ce391d3773a0ac06238c703484484883a3a0d7c
Bruno Sousa <brunosousa.software@gmail.com>
2025-04-03 19:35:08 +0100

    +fix(Array input)

--- 1 - Two Sum/Solution.java
+++ 1 - Two Sum/Solution.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -8,5 +11,6 @@ class Solution {
             }
             map.put(nums[i], i);
         }
+        return nums;
     }
 }
